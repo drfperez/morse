3ad51eba1d1fd8be1a9b0673443f1dc99ba2e012
Francisco Pérez <drfperez@gmail.com>
2022-04-27 13:23:53 +0200

    New file

---
+++ morse.ino
@@ -0,0 +1,74 @@
+// Morse code for blinking a LED
+// Remember to add speaker code by declaring a new variable for your 8 Ohms 0.5W speaker in e.g. pin 8 in Arduino Uno, or e.g. pin 2 in ESP32
+// If you are using ESP8266 or ESP32 remember to add the general corresponding libraries in File/Preferences/ URL:
+// https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_index.json, http://arduino.esp8266.com/stable/package_esp8266com_index.json
+// Then go for Tools/Board manager and install your microcontroller driver (ESP32 or ESP8266)
+// For ESP32 install the library ESP32Servo found in Arduino IDE containing the instruction tone (speakerpin, frequency); and noTone(speakerpin);
+int ledPin = 13;
+int dotDelay = 200;
+
+char* letters[] = {
+  ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..",    // A-I
+  ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.",  // J-R
+  "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."          // S-Z
+};
+
+char* numbers[] = {
+  "-----", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----."};
+
+void setup()                 
+{
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop()                    
+{
+  char ch;
+  if (Serial.available() > 0)
+  {
+    ch = Serial.read();
+    if (ch >= 'a' && ch <= 'z')
+    {
+      flashSequence(letters[ch - 'a']);
+    }
+    else if (ch >= 'A' && ch <= 'Z')
+    {
+      flashSequence(letters[ch - 'A']);
+    }
+    else if (ch >= '0' && ch <= '9')
+    {
+      flashSequence(numbers[ch - '0']);
+    }
+    else if (ch == ' ')
+    {
+      delay(dotDelay * 4);  // gap between words  
+    }
+  }
+}
+
+void flashSequence(char* sequence)
+{
+  int i = 0;
+  while (sequence[i] != NULL)
+  {
+    flashDotOrDash(sequence[i]);
+    i++;
+  }
+  delay(dotDelay * 3);    // gap between letters
+}
+
+void flashDotOrDash(char dotOrDash)
+{
+  digitalWrite(ledPin, HIGH);
+  if (dotOrDash == '.')
+  {
+    delay(dotDelay);           
+  }
+  else // must be a dash
+  {
+    delay(dotDelay * 3);           
+  }
+  digitalWrite(ledPin, LOW);    
+  delay(dotDelay); // gap between flashes
+}
